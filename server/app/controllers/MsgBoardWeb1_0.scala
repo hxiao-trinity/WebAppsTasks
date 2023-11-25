d323d26c9ce479831ab9854c7277e66a6ae70d7d
H X <hxiao@trinity.edu>
2023-11-24 19:22:03 -0600

    Made public and private messages work

--- server/app/controllers/MsgBoardWeb1_0.scala
+++ server/app/controllers/MsgBoardWeb1_0.scala
@@ -10,11 +10,11 @@ import javax.inject._
 case class User(username:String, password:String)
 
 class MsgBoardWeb1_0 @Inject() (cc: MessagesControllerComponents) extends MessagesAbstractController(cc){
-
-    def taskList() = Action {
+/*
+    def msgBoard() = Action {
         Ok(views.html.MsgBoardWeb1_0(Nil))
     }
-
+*/
     def validateLoginGet(username:String, password:String) = Action {
         Ok(s"$username logged in with password $password")
     }
@@ -25,14 +25,51 @@ class MsgBoardWeb1_0 @Inject() (cc: MessagesControllerComponents) extends Messag
             val username = args("username").head
             val password = args("password").head
             if (models.MsgBoardModel1.validateUser(username,password)) // if credential matches
-                Redirect(routes.MsgBoardWeb1_0.taskList)    //let it in
+                Redirect(routes.MsgBoardWeb1_0.msgBoard).withSession("username" -> username)    //let it in
             else
                 Redirect(routes.MsgBoardWeb1_0.login)   //redirect to login page
         }.getOrElse(Ok(""))
-  }
+    }
 
     def login() = Action {
         Ok(views.html.login(Nil))
+    }
+
+    def createUser = Action { implicit request =>
+        val postVals = request.body.asFormUrlEncoded
+        postVals.map { args =>
+            val username = args("username").head
+            val password = args("password").head
+            if (models.MsgBoardModel1.createUser(username,password)) // if credential matches
+                Redirect(routes.MsgBoardWeb1_0.msgBoard).withSession("username" -> username)     //let it in
+            else
+                Redirect(routes.MsgBoardWeb1_0.login)   //redirect to login page
+        }.getOrElse(Ok(""))
+    }
+
+
+    /////NEW CODES
+    def postMessage = Action { implicit request =>
+        val postVals = request.body.asFormUrlEncoded
+        postVals.map { args =>
+            val username = request.session.get("username").getOrElse("anonymous")
+            val content = args("content").head
+            models.MsgBoardModel1.sendMsg(username, content)
+            Redirect(routes.MsgBoardWeb1_0.msgBoard)
+        }.getOrElse(Redirect(routes.MsgBoardWeb1_0.msgBoard))
+    }
+
+    def msgBoard() = Action { implicit request =>
+        val usernameOption = request.session.get("username")
+        usernameOption.map{username => 
+            val msgs = models.MsgBoardModel1.getMessages(username) ++ models.MsgBoardModel1.getPublicMessages(username)
+            Ok(views.html.MsgBoardWeb1_0(msgs))
+        }.getOrElse(Redirect(routes.MsgBoardWeb1_0.login))
+        
+
+//        val username = request.session.get("username").getOrElse("")
+//        val messages = models.MsgBoardModel1.getMessages(username) ++ models.MsgBoardModel1.getPublicMessages()
+        
     }
 
 }
--- server/app/models/MsgBoardModel1.scala
+++ server/app/models/MsgBoardModel1.scala
@@ -1,11 +1,14 @@
 package models
 
-import scala.collection.mutable.Map
+import scala.collection.mutable
 
 object MsgBoardModel1 {
 
-    private var users = Map[String,String]("mark" -> "lewis", "web" -> "apps")
+    // A simple message structure.
+    case class Message(content:String, from:String, to:Option[String] = None)
 
+    private var users = mutable.Map[String,String]("mlewis" -> "prof", "web" -> "apps")
+    private var msgs = mutable.ListBuffer[Message](Message("Hello, Web!", "mlewis", Some("web")), Message("Hello, people!", "web"))
 
     def validateUser(username:String, password:String) : Boolean = {
         users.get(username).map(_ == password).getOrElse(false)
@@ -20,7 +23,22 @@ object MsgBoardModel1 {
         }
     }
 
-    def sendMsg(msg:String) = {
-        ???
+    def sendMsg(username: String, content: String): Unit = {
+        msgs += Message(content, username, Some(username))
     }
+
+    def getMessagesFrom(username: String): Seq[Message] = {
+        msgs.filter(_.from.contains(username)).toSeq
+    }
+
+    //Get all PUBLIC messages. If that one is from username, count ONCE.
+    def getPublicMessages(username:String): Seq[Message] = { 
+        var temp = (msgs.filter(_.to.isEmpty)).filterNot((_.from.equals(username))).toSeq
+        temp
+    }
+
+    def getMessages(username:String) :Seq[Message] = {
+        getMessagesFrom(username) ++ getPublicMessages(username)
+    }
+
 }
