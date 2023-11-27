5bc7b7bf6c2380074e0aec36c0599262633cf8bb
H X <hxiao@trinity.edu>
2023-11-26 21:03:41 -0600

    task6 - doing browser test

--- server/test/MsgBoardWeb1_0Spec.scala
+++ server/test/MsgBoardWeb1_0Spec.scala
@@ -8,12 +8,16 @@ class MsgBoardWeb1_0Specs extends PlaySpec with GuiceOneServerPerSuite with OneB
 
 
         "login and access functions" in {
-            go to s"http://localhost:$port/login"
-            click on "username-login"
-            textField("username-login").value = "mlewis"
-            click on "password-login"
-            pwdField("password-login").value = "prof"
-            submit()
+            
+            go to s"http://localhost:9000/login"
+            //eventually{
+                find("h2").foreach(e => e.text mustBe("Login"))
+                click on "username-login"
+                textField("username-login").value = "mlewis"
+                click on "password-login"
+                pwdField("password-login").value = "prof"
+                submit()
+            //}
         }
 
 
