73d5b1ff40914acf1a81c1c9fdc2d0109a103f7a
H X <hxiao@trinity.edu>
2023-11-27 02:34:49 -0600

    task7 - trivial

--- server/public/javascripts/app.js
+++ server/public/javascripts/app.js
@@ -1,17 +1,17 @@
 
     const canvas = document.getElementById('gameCanvas');
     const ctx = canvas.getContext('2d');
-    const socket = new WebSocket('ws://localhost:9000/WSGame'); // Replace with your server address
+    const socket = new WebSocket('ws://localhost:9000/WSGame');
 
-    let player = { x: 50, y: 50, id: 'unique_player_id' }; // Add a unique identifier for each player
+    let player = { x: 50, y: 50, id: 1 }; 
 
-    // Handle WebSocket Messages
+  
     socket.onmessage = function(event) {
         const players = JSON.parse(event.data);
         drawPlayers(players);
     };
 
-    // Event Listener for Arrow Keys
+  
     document.addEventListener('keydown', function(event) {
         switch(event.key) {
             case 'ArrowUp': player.y -= 5; break;
@@ -25,16 +25,16 @@
 
     // Draw Player Function
     function drawPlayer(player) {
-        ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
-        ctx.fillStyle = 'blue'; // Set player color
-        ctx.fillRect(player.x, player.y, 10, 10); // Draw the player
+        ctx.clearRect(0, 0, canvas.width, canvas.height); 
+        ctx.fillStyle = 'blue'; 
+        ctx.fillRect(player.x, player.y, 10, 10); 
     }
 
     // Draw All Players
     function drawPlayers(players) {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         players.forEach(p => {
-            ctx.fillStyle = p.id === player.id ? 'blue' : 'red'; // Differentiate the current player
+            ctx.fillStyle = p.id === player.id ? 'blue' : 'red'; 
             ctx.fillRect(p.x, p.y, 10, 10);
         });
     }
