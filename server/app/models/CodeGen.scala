dcd962b856cdfd280d42a98c905f1153ed3022c2
2020-08-18 <hxiao@pandora05.cs.trinity.edu>
2023-12-07 15:20:42 -0600

    task9 - midify CodeGen.scala to make it work

--- server/app/models/CodeGen.scala
+++ server/app/models/CodeGen.scala
@@ -4,8 +4,8 @@ object CodeGen extends App {
   slick.codegen.SourceCodeGenerator.run(
     "slick.jdbc.PostgresProfile",
     "org.postgresql.Driver",
-    "jdbc:postgresql://pandora05.cs.trinity.edu/hxiao?user=hxiao&password=0851643",
-    "users/hxiao/Local/HTML-Documents/WebApps/server/app",
+    "jdbc:postgresql://localhost/hxiao?user=hxiao&password=0851643",
+    "/users/hxiao/Local/HTML-Documents/WebApps/server/app",
     "models", None, None, true, false
   )
 }
