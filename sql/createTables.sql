850968913889d83886092a07778f958ed32362e8
H X <hxiao@trinity.edu>
2023-12-06 23:15:29 -0600

    task9 - adding sent_at column to db

--- sql/createTables.sql
+++ sql/createTables.sql
@@ -1,12 +1,13 @@
 CREATE TABLE users (
-                       id SERIAL PRIMARY KEY,
-                       username varchar NOT NULL,
-                       password varchar NOT NULL
+    id SERIAL PRIMARY KEY,
+    username varchar NOT NULL,
+    password varchar NOT NULL
 );
 
 CREATE TABLE messages (
-                       message_id SERIAL PRIMARY KEY,
-                       from_user_id int NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-                       content varchar,
-                       to_user_id int REFERENCES users(id) ON DELETE CASCADE
+    message_id SERIAL PRIMARY KEY,
+    from_user_id int NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    content varchar,
+    to_user_id int REFERENCES users(id) ON DELETE CASCADE,
+    sent_at timestamp DEFAULT current_timestamp
 );
