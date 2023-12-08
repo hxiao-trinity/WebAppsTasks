80a5c0d3ee29978571a91471ddd1044df297fc6b
H X <hxiao@trinity.edu>
2023-12-07 23:52:21 -0600

    task9 - modify schema

--- sql/createTables.sql
+++ sql/createTables.sql
@@ -1,13 +1,12 @@
 CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    username varchar NOT NULL,
-    password varchar NOT NULL
+    username varchar(20) PRIMARY KEY,
+    password varchar(200) NOT NULL
 );
 
 CREATE TABLE messages (
     message_id SERIAL PRIMARY KEY,
-    from_user_id int NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-    content varchar,
-    to_user_id int REFERENCES users(id) ON DELETE CASCADE,
+    from_user varchar(20) NOT NULL REFERENCES users(username) ON DELETE CASCADE,
+    content varchar(2000),
+    to_user varchar(20) REFERENCES users(username) ON DELETE CASCADE,
     sent_at timestamp DEFAULT current_timestamp
 );
