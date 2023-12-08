3c29f5003a952432049ccc7fa731e2baf68e9f35
H X <hxiao@trinity.edu>
2023-12-08 04:07:19 -0600

    task8 - can login at /load2

--- server/app/controllers/MsgBoardAjax.scala
+++ server/app/controllers/MsgBoardAjax.scala
@@ -1,10 +1,11 @@
 package controllers
 
+import models.MsgBoardModel1
 import play.api.data.Forms._
 import play.api.data._
 import play.api.i18n._
 import play.api.mvc._
-import models.MsgBoardModel1
+
 import javax.inject._
 
 @Singleton
@@ -13,11 +14,11 @@ class MsgBoardAjax @Inject() (cc: MessagesControllerComponents) extends Messages
         Ok(views.html.MsgBoardAjaxMain())
     }
 
-    def login = Action{
+    def login() = Action{ implicit request =>
         Ok(views.html.login2())
     }
 
-    def validate(username:String, password:String) = Action{
+    def validate(username:String, password:String) = Action{ implicit request =>
         if (MsgBoardModel1.validateUser(username, password)) 
             Ok(views.html.MsgBoardAjax(username, MsgBoardModel1.getMessages(username))).withSession("username" -> username)
         else
--- server/public/javascripts/MsgBoardAjax.js
+++ server/public/javascripts/MsgBoardAjax.js
@@ -1,15 +1,22 @@
-$("#contents").load("/login2")
 
+
+$("#contents").load("/login2")
+console.log("MsgBoardAjax.js loaded");
 
 function login() {
-    const username = $("#loginName").val()
-    const password = $("#loginPass").val()
-    $("contents").load("/validate2?username=" + username + "&password=" + password);
-    console.log("Trying to login with " + username + ", " + password)
+    console.log("Try to login");
+    
+    const username = $("#loginName").val();
+    const password = $("#loginPass").val();
+    $("#contents").load("/validate2?username=" + username + "&password=" + password);
+    console.log("Trying to login with " + username + ", " + password);
+    
 }
 
 function createUser() {
-    const username = $("#createName").val()
-    const password = $("#createPass").val()
+    
+    const username = $("#createName").val();
+    const password = $("#createPass").val();
     console.log("Trying to create user with " + username + ", " + password)
+    
 }
