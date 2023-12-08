95903139926dcb88a5675ce679cf666b8af7a7f5
H X <hxiao@trinity.edu>
2023-12-08 01:11:24 -0600

    start react version

---
+++ server/app/controllers/MsgBoardReact.scala
@@ -0,0 +1,17 @@
+package controllers
+
+import play.api.data.Forms._
+import play.api.data._
+import play.api.i18n._
+import play.api.mvc._
+
+import javax.inject._
+
+@Singleton
+class MsgBoardReact @Inject() (cc: MessagesControllerComponents) extends MessagesAbstractController(cc){
+
+    def load() = TODO
+
+    def data() = TODO
+
+}
